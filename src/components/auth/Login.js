8001ae2de1b77ec80d34f97dd2e529dea6706638
Nick Hosley <nhosley1976@gmail.com>
2018-10-16 12:46:13 -0700

    changed some redirect stuff

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -37,7 +37,7 @@ export default withAuth(
     render() {
       if (this.state.authenticated === null) return null;
       return this.state.authenticated ? (
-        <Redirect to={{ pathname: '/' }} />
+        <Redirect to={{ pathname: 'https://project3bc.herokuapp.com' }} />
       ) : (
         <SignInWidget
           baseUrl={this.props.baseUrl}
