5e946b4988692b55e10bc778e439fe7690d9a17d
Nick Hosley <nhosley1976@gmail.com>
2018-10-24 13:23:50 -0700

    changed client_id for site

--- src/App.js
+++ src/App.js
@@ -22,7 +22,8 @@ class App extends Component {
         <Router>
          <Security
           issuer="https://dev-132516.oktapreview.com/oauth2/default"
-          client_id="0oaglojav30K9El7a0h7"
+          // client_id="0oaglojav30K9El7a0h7"
+          client_id="0oagwqexlfq4CEAEX0h7"
           redirect_uri={window.location.origin + '/implicit/callback'}
           onAuthRequired={onAuthRequired}
         >
