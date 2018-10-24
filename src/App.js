fe53be593bf24e80c2aca40052ae2cdcdce8438b
Nick Hosley <nhosley1976@gmail.com>
2018-10-24 13:29:47 -0700

    status changed took back changes

--- src/App.js
+++ src/App.js
@@ -22,8 +22,7 @@ class App extends Component {
         <Router>
          <Security
           issuer="https://dev-132516.oktapreview.com/oauth2/default"
-          // client_id="0oaglojav30K9El7a0h7"
-          client_id="0oagwqexlfq4CEAEX0h7"
+          client_id="0oaglojav30K9El7a0h7"
           redirect_uri={window.location.origin + '/implicit/callback'}
           onAuthRequired={onAuthRequired}
         >
@@ -35,8 +34,7 @@ class App extends Component {
                 <SecureRoute path="/Recipe" exact={true} component={Recipe} />
                 <SecureRoute path="/about" exact={true} component={About} />
                 <Route path="/login" render={() => (
-                  // <Login baseUrl="https://dev-132516.oktapreview.com" />
-                  <Login baseUrl="https://modest-wing-fe7798.netlify.com/" />
+                  <Login baseUrl="https://dev-132516.oktapreview.com" />
                 )}
               />
               <Route path="/implicit/callback" component={ImplicitCallback} />
